e307a51ff7f6e9c1322f8c472dafeceb6cb55a3f
Aleksey <web.ams@yandex.ru>
2023-09-21 13:52:31 +0300

    создал и зарегистрировал модели Жанр и Режиссеры

--- Mysite/films/admin.py
+++ Mysite/films/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Film
+from .models import Film, Genre, Direction
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_publ')
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Direction)
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
--- Mysite/films/models.py
+++ Mysite/films/models.py
@@ -1,11 +1,40 @@
 from django.db import models
 
+class Genre(models.Model):
+    '''жанры фильмов'''
+    name = models.CharField('Название жанра', max_length=50)
+    description = models.TextField('Описание', blank=True)
+    url = models.SlugField(max_length=50, unique=True) # SlugField отвечает за адрес страницы, unique - уникальная
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+class Direction(models.Model):
+    '''Режиссеры'''
+    name = models.CharField('Фамилия и Имя', max_length=100)
+    description = models.TextField('Биография', blank=True)
+    image = models.ImageField('Фотография', upload_to='image/directions/%Y')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Режиссер'
+        verbose_name_plural = 'Режиссеры'
+
 class Film(models.Model):
     '''информация о фильме'''
-    title = models.CharField(max_length = 100)
-    imag = models.ImageField(upload_to='image/&Y')
+    title = models.CharField('Название фильма', max_length = 100)
+    imag = models.ImageField('Постер', upload_to='image/&Y')
     description = models.TextField(blank=True)
     date_publ = models.DateField('Дата выхода')
+    directions = models.ManyToManyField(Direction, verbose_name='Режиссеры')
+    genre = models.ManyToManyField(Genre, verbose_name='Жанры')
+    url = models.SlugField(max_length=100, unique=True)
 
     def __str__(self):
         return f'{self.title}, {self.date_publ}'
