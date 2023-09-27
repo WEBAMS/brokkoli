8e08f00a898f5a2554d9f50d2d74a32559690aae
Aleksey <web.ams@yandex.ru>
2023-09-27 16:24:43 +0300

    Устанавливаем ссылки

--- Mysite/films/urls.py
+++ Mysite/films/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FilmsView.as_view())
+    path('', views.FilmsView.as_view()),
+    path('<slug:slug>/', views.FilmDetail.as_view())
 ]
--- Mysite/films/views.py
+++ Mysite/films/views.py
@@ -8,4 +8,10 @@ class FilmsView(View):
         films = Film.objects.all()
         return render(request, 'films/film.html', {'film_list': films})
 
+class FilmDetail(View):
+    '''Представление одного фильма'''
+    def get(self, request, slug):
+        film = Film.objects.get(url=slug)
+        return render(request, 'films/film_detail.html', {'film': film})
+
 
