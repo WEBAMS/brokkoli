e8822f6a4c3a9b55e8bed15058c95cc054492d15
Aleksey <web.ams@yandex.ru>
2023-10-26 13:02:44 +0300

    Завершение проекта

--- Mysite/Mysite/settings.py
+++ Mysite/Mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'films.apps.FilmsConfig'
+    'films.apps.FilmsConfig',
     'accounts.apps.AccountsConfig'
 ]
 
--- Mysite/Mysite/urls.py
+++ Mysite/Mysite/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('films.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 ]
 
